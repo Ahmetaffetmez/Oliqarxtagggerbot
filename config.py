ac70f309aa9ab607ac5eecd9716edee83edc805f
Ahmetaffetmez <147531181+Ahmetaffetmez@users.noreply.github.com>
2024-08-18 20:28:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ bot_token = "7120940125:AAESL30SRo8CFIooHy7J2KYiyv7KYL5qn8M" #botfatherdan alabi
 client = TelegramClient("Samil", api_id, api_hash).start(bot_token=bot_token)
 
 USERNAME = "@Sohbet_oyunbot" #botunuzun kullanıcı adı
-log_qrup = "1001906045085 #log qrupunuzun idsi
+log_qrup = -1001906045085 #log qrupunuzun idsi
 startmesaj = "💌 arkadaşlarınızı nasıl etiketlemek istersiniz ...\n📚 ben gruplarınız için Ahmet tarafından özel geliştirilen bir botum" #start mesajınız
 komutlar = "🇦🇿 Bütün etiketler ;\n\n» /utag   <  mesaj  >\n   - üyeleri 5'li etiketler .\n\n» /tag   <  mesaj  >\n   - üyeleri tek tek etiketler .\n\n» /atag   <  mesaj  >\n   - Adminleri etiketler .\n\n» /etag   <  mesaj  >\n   - üyeleri emoji ile etiketler .\n\n» /stag   <  mesaj  >\n   - üyeleri güzel söz ile etiketler .\n\n» /cancel = >\n   - etiketleme işlemi durdurulur ." #komutların olduğu mesaj
 qrupstart = "• şuan aktif çalışmaktayım  . . .\n\n• üyeleri etiketlemek için grubunuza ekleyin . . ." #aktif olduğunda gruba gelen mesaj
