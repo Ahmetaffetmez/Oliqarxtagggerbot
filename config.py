4df71d090bed1458478a4f8b958f3c977a92ba1e
Ahmetaffetmez <147531181+Ahmetaffetmez@users.noreply.github.com>
2024-10-05 07:48:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -39,17 +39,17 @@ import random
 # 
 api_id = "26993866" #my.telegram.org/apps adresinden alabilirsiniz 
 api_hash = "1b769848d054cce9eba9c5f517fbceb8" #my.telegram.org/apps adresinden alabilirsiniz
-bot_token = "6946687913:AAFvIZtHDEA9MHIo1fLrQgRXEc9C6CebYDw" #botfatherdan alabilirsiniz
+bot_token = "7641153006:AAEQlh44webKoTmwVn0A2ouM7gEXNcqiiS0" #botfatherdan alabilirsiniz
 
 client = TelegramClient("Samil", api_id, api_hash).start(bot_token=bot_token)
 
-USERNAME = "aidenetiket_bot" #botunuzun kullanıcı adı
-log_qrup = -1001984404074 #log qrupunuzun idsi
-startmesaj = "💌 arkadaşlarınızı nasıl etiketlemek istersiniz ...\n📚 ben gruplarınız için çavreş tarafından özel geliştirilen bir botum" #start mesajınız
+USERNAME = "hacer_tag_bot" #botunuzun kullanıcı adı
+log_qrup = -1001906045085 #log qrupunuzun idsi
+startmesaj = "💌 arkadaşlarınızı nasıl etiketlemek istersiniz ...\n📚 ben gruplarınız için Ahmet tarafından hacer icin özel geliştirilen bir botum" #start mesajınız
 komutlar = "🇦🇿 Bütün etiketler ;\n\n» /utag   <  mesaj  >\n   - üyeleri 5'li etiketler .\n\n» /tag   <  mesaj  >\n   - üyeleri tek tek etiketler .\n\n» /atag   <  mesaj  >\n   - Adminleri etiketler .\n\n» /etag   <  mesaj  >\n   - üyeleri emoji ile etiketler .\n\n» /stag   <  mesaj  >\n   - üyeleri güzel söz ile etiketler .\n\n» /cancel = >\n   - etiketleme işlemi durdurulur ." #komutların olduğu mesaj
 qrupstart = "• şuan aktif çalışmaktayım  . . .\n\n• üyeleri etiketlemek için grubunuza ekleyin . . ." #aktif olduğunda gruba gelen mesaj
-support = "https://t.me/Kraliyet_ailesii" #destek qrupunuzun kullanıcı adı
-sahib = "AIDEN34" #sahibinizin kullanıcı adı
+support = "https://t.me/affetmezler_sohbet" #destek qrupunuzun kullanıcı adı
+sahib = "kurucu_sahipp" #sahibinizin kullanıcı adı
 noadmin = "➻ Üzgünüm Ama Yönetici Değilsiniz ." #yönetici olmayanlar için mesaj
 
 #
