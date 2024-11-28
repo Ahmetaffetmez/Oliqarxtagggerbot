372fb4ab701577cd4a30e4f6c6152b8f90dee96d
Ahmetaffetmez <147531181+Ahmetaffetmez@users.noreply.github.com>
2024-11-28 06:52:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -39,11 +39,11 @@ import random
 # 
 api_id = "23342734" #my.telegram.org/apps adresinden alabilirsiniz 
 api_hash = "82ff606d717557e233b3bfdf82e0e74e" #my.telegram.org/apps adresinden alabilirsiniz
-bot_token = "7283252508:AAHoQEmOxutEePwZ3Nl_mIK09bu_nfJHceU" #botfatherdan alabilirsiniz
+bot_token = "8094013638:AAGkV5ktnUCmaQlZ951B0paq_0ai--coYFw" #botfatherdan alabilirsiniz
 
 client = TelegramClient("Samil", api_id, api_hash).start(bot_token=bot_token)
 
-USERNAME = "Tako_etiket_bot" #botunuzun kullanıcı adı 
+USERNAME = "Baveteyar_etiket_bot" #botunuzun kullanıcı adı 
 log_qrup = "-1001906045085", #log qrupunuzun idsi
 startmesaj = "💌 arkadaşlarınızı nasıl etiketlemek istersiniz ...\n📚 ben gruplarınız için ahmet tarafından özel geliştirilen bir botum" #start mesajınız
 komutlar = "🇦🇿 Bütün etiketler ;\n\n» /utag   <  mesaj  >\n   - üyeleri 5'li etiketler .\n\n» /tag   <  mesaj  >\n   - üyeleri tek tek etiketler .\n\n» /atag   <  mesaj  >\n   - Adminleri etiketler .\n\n» /etag   <  mesaj  >\n   - üyeleri emoji ile etiketler .\n\n» /stag   <  mesaj  >\n   - üyeleri güzel söz ile etiketler .\n\n» /cancel = >\n   - etiketleme işlemi durdurulur ." #komutların olduğu mesaj
