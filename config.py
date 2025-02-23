e9e194e70bc61dcb947e10e17bdab464092cf5f5
Ahmetaffetmez <147531181+Ahmetaffetmez@users.noreply.github.com>
2025-02-23 08:17:13 +0300

    Update config.py

--- config.py
+++ config.py
@@ -39,13 +39,13 @@ import random
 # 
 api_id = "23342734" #my.telegram.org/apps adresinden alabilirsiniz 
 api_hash = "82ff606d717557e233b3bfdf82e0e74e" #my.telegram.org/apps adresinden alabilirsiniz
-bot_token = "7676862150:AAGlls4NxrkE7zqToaK-uvKNg8CUGAezIIA" #botfatherdan alabilirsiniz
+bot_token = "8027708363:AAF290mVHzbM5HP-_3qCn8IWZYEWtuwLsOI" #botfatherdan alabilirsiniz
 
 client = TelegramClient("Samil", api_id, api_hash).start(bot_token=bot_token)
 
-USERNAME = "Baretli_music_bot" #botunuzun kullanıcı adı 
+USERNAME = "Black_Chat_tagbot" #botunuzun kullanıcı adı 
 log_qrup = "-1001906045085", #log qrupunuzun idsi
-startmesaj = "💌 arkadaşlarınızı nasıl etiketlemek istersiniz ...\n📚 ben gruplarınız için ahmet tarafından özel geliştirilen bir botum" #start mesajınız
+startmesaj = "💌 arkadaşlarınızı nasıl etiketlemek istersiniz ...\n📚 "ben gruplarınız için ahmet tarafından özel geliştirilen bir botum" #start mesajınız
 komutlar = "🇦🇿 Bütün etiketler ;\n\n» /utag   <  mesaj  >\n   - üyeleri 5'li etiketler .\n\n» /tag   <  mesaj  >\n   - üyeleri tek tek etiketler .\n\n» /atag   <  mesaj  >\n   - Adminleri etiketler .\n\n» /etag   <  mesaj  >\n   - üyeleri emoji ile etiketler .\n\n» /stag   <  mesaj  >\n   - üyeleri güzel söz ile etiketler .\n\n» /cancel = >\n   - etiketleme işlemi durdurulur ." #komutların olduğu mesaj
 qrupstart = "• şuan aktif çalışmaktayım  . . .\n\n• üyeleri etiketlemek için grubunuza ekleyin . . ." #aktif olduğunda gruba gelen mesaj
 support = "https://t.me/kurtlar_sofrasi_oyun" #destek qrupunuzun kullanıcı adı
